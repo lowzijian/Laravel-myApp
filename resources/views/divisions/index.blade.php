52c6f3ee05823335f749f8e21a5918bf657c8efa
lowzijian <lowbak1998@hotmail.com>
2020-02-05 15:30:34 +0800

    Add in create button

--- resources/views/divisions/index.blade.php
+++ resources/views/divisions/index.blade.php
@@ -1,6 +1,6 @@
 <?php
 
-	use App\Common;
+use App\Common;
 
 ?>
 @extends('layouts.app')
@@ -8,79 +8,82 @@
 @section('content')
 <!-- Bootstrap Boilerplate... -->
 <div class="panel-body">
-@if (count($divisions) > 0)
-<table class="table table-striped task-table">
-<!-- Table Headings -->
-<thead>
-<tr>
-<th>No.</th>
-<th>Code</th>
-<th>Name</th>
-<th>City</th>
-<th>State</th>
-<th>Created</th>
-<th>Actions</th>
-</tr>
-</thead>
+	@if (count($divisions) > 0)
+	<table class="table table-striped task-table">
+		<!-- Table Headings -->
+		<thead>
+			<tr>
+				<th>No.</th>
+				<th>Code</th>
+				<th>Name</th>
+				<th>City</th>
+				<th>State</th>
+				<th>Created</th>
+				<th>Actions</th>
+			</tr>
+		</thead>
 
-<!-- Table Body -->
-<tbody>
-@foreach ($divisions as $i => $division)
-<tr>
-<td class="table-text">
-<div>{{ $i+1 }}</div>
-</td>
-<td class="table-text">
-<div>
-{!! link_to_route(
-'division.show',
-$title = $division->code,
-$parameters = [
-'id' => $division->id,
-]
-) !!}
-</div>
-</td>
-<td class="table-text">
-<div>{{ $division->name }}</div>
-</td>
-<td class="table-text">
-<div>{{ $division->city }}</div>
-</td>
-<td class="table-text">
-<div>{{ Common::$states[$division->state] }}</div>
-</td>
-<td class="table-text">
-<div>{{ $division->created_at }}</div>
-</td>
-<td class="table-text">
-<div>
-{!! link_to_route(
-'division.edit',
-$title = 'Edit',
-$parameters = [
-'id' => $division->id,
-]
-) !!}
-|
-<!--{!!
+		<!-- Table Body -->
+		<tbody>
+			@foreach ($divisions as $i => $division)
+			<tr>
+				<td class="table-text">
+					<div>{{ $i+1 }}</div>
+				</td>
+				<td class="table-text">
+					<div>
+						{!! link_to_route(
+						'division.show',
+						$title = $division->code,
+						$parameters = [
+						'id' => $division->id,
+						]
+						) !!}
+					</div>
+				</td>
+				<td class="table-text">
+					<div>{{ $division->name }}</div>
+				</td>
+				<td class="table-text">
+					<div>{{ $division->city }}</div>
+				</td>
+				<td class="table-text">
+					<div>{{ Common::$states[$division->state] }}</div>
+				</td>
+				<td class="table-text">
+					<div>{{ $division->created_at }}</div>
+				</td>
+				<td class="table-text">
+					<div>
+						{!! link_to_route(
+						'division.edit',
+						$title = 'Edit',
+						$parameters = [
+						'id' => $division->id,
+						]
+						) !!}
+						|
+						<!--{!!
  Form::open(['method' => 'DELETE', 'route' => ['division.destroy', $division->id]]) 
 !!}
 {!!
 	Form::submit('Delete')
 !!}
 -->
-<a href="{{ route('division.delete', $division->id) }}">Delete</a>
-</div>
-</td>
-</tr>
-@endforeach
-</tbody>
-</table>
-@else
-<div>
-No records found
-</div>
-@endif
+						<a href="{{ route('division.delete', $division->id) }}">Delete</a>
+					</div>
+				</td>
+			</tr>
+			@endforeach
+		</tbody>
+	</table>
+	<div class="col-sm-12">
+		<button type="button" style="float:right;" onclick=" window.location.href= `{{ url('division/create')}}`">Add Division</button>
+	</div>
+	@else
+	<div>
+		No records found
+	</div>
+	@endif
 </div>
 @endsection
--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -1,6 +1,6 @@
 <?php
 
-	use App\Common;
+use App\Common;
 
 ?>
 @extends('layouts.app')
@@ -8,57 +8,57 @@
 @section('content')
 <!-- Bootstrap Boilerplate... -->
 <div class="panel-body">
-@if (count($groups) > 0)
-<table class="table table-striped task-table">
-<!-- Table Headings -->
-<thead>
-<tr>
-<th>No.</th>
-<th>Code</th>
-<th>Name</th>
-<th>Description</th>
-<th>Created</th>
-<th>Actions</th>
-</tr>
-</thead>
+	@if (count($groups) > 0)
+	<table class="table table-striped task-table">
+		<!-- Table Headings -->
+		<thead>
+			<tr>
+				<th>No.</th>
+				<th>Code</th>
+				<th>Name</th>
+				<th>Description</th>
+				<th>Created</th>
+				<th>Actions</th>
+			</tr>
+		</thead>
 
-<!-- Table Body -->
-<tbody>
-@foreach ($groups as $i => $group)
-<tr>
-<td class="table-text">
-<div>{{ $i+1 }}</div>
-</td>
-<td class="table-text">
-<div>
-{!! link_to_route(
-'group.show',
-$title = $group->code,
-$parameters = [
-'id' => $group->id,
-]
-) !!}
-</div>
-</td>
-<td class="table-text">
-<div>{{ $group->name }}</div>
-</td>
-<td class="table-text">
-<div>{{ $group->description }}</div>
-</td>
-<td class="table-text">
-<div>{{ $group->created_at }}</div>
-</td>
-<td class="table-text">
-<div>
-{!! link_to_route(
-'group.edit',
-$title = 'Edit',
-$parameters = [
-'id' => $group->id,
-]
-) !!}
-<!--
+		<!-- Table Body -->
+		<tbody>
+			@foreach ($groups as $i => $group)
+			<tr>
+				<td class="table-text">
+					<div>{{ $i+1 }}</div>
+				</td>
+				<td class="table-text">
+					<div>
+						{!! link_to_route(
+						'group.show',
+						$title = $group->code,
+						$parameters = [
+						'id' => $group->id,
+						]
+						) !!}
+					</div>
+				</td>
+				<td class="table-text">
+					<div>{{ $group->name }}</div>
+				</td>
+				<td class="table-text">
+					<div>{{ $group->description }}</div>
+				</td>
+				<td class="table-text">
+					<div>{{ $group->created_at }}</div>
+				</td>
+				<td class="table-text">
+					<div>
+						{!! link_to_route(
+						'group.edit',
+						$title = 'Edit',
+						$parameters = [
+						'id' => $group->id,
+						]
+						) !!}
+						<!--
 {!!
  Form::open(['method' => 'DELETE', 'route' => ['group.destroy', $group->id]]) 
 !!}
@@ -66,18 +66,21 @@ $parameters = [
 	Form::submit('Delete')
 !!}
 -->
-|
-<a href="{{ route('group.delete', $group->id) }}">Delete</a>
-</div>
-</td>
-</tr>
-@endforeach
-</tbody>
-</table>
-@else
-<div>
-No records found
-</div>
-@endif
+						|
+						<a href="{{ route('group.delete', $group->id) }}">Delete</a>
+					</div>
+				</td>
+			</tr>
+			@endforeach
+		</tbody>
+	</table>
+	<div class="col-sm-12">
+		<button type="button" style="float:right;" onclick=" window.location.href= `{{ url('group/create')}}`">Add Group</button>
+	</div>
+	@else
+	<div>
+		No records found
+	</div>
+	@endif
 </div>
 @endsection
