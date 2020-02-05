d2321bfc81306bb3fdf6890100f82732e52230e9
lowzijian <lowbak1998@hotmail.com>
2020-02-05 15:26:48 +0800

    Remove styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,12 +10,6 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
-    <!-- CSS And JavaScript -->
-    <style>
-        body {
-            margin: 0;
-        }
-    </style>
 </head>
 <header style="height:25px;padding:5px">
     <nav class="col-md-12">
